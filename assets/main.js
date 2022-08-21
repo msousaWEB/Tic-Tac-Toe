0c86a59d6b779230efd0ad0ad1d68ead76d0cd76
msousa <msousa.dev@outlook.com>
2022-08-21 18:03:23 -0300

    structuring the logic of the plays

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,47 @@
-console.log('ola');
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let btn = document.querySelectorAll("#btn-container button");
+let msgContainer = document.querySelector("#message");
+let msgText = document.querySelector("#message p");
+let secondPlayer;
+
+// play counter
+let player1 = 0;
+let player2 = 0;
+
+// adding click event in each box
+for(let i = 0; i < boxes.length; i++) {
+    // when someone clicks on the box
+    boxes[i].addEventListener("click", function() {
+        let element = checkPlayer(player1, player2);
+
+        // setting play limit
+        if(this.childNodes.length == 0 ) {
+            let cloneElement = element.cloneNode(true);
+            this.appendChild(cloneElement);
+
+            // computing play
+            if(player1 == player2) {
+                player1++;
+            } else {
+                player2++;
+            }
+        }
+
+    });
+
+}
+
+function checkPlayer(player1, player2) {
+    // defining whose turn it is to play
+    if(player1 == player2) {
+        //x
+        element = x;
+    } else {
+        //o
+        element = o;
+    }
+
+    return element;
+}
